e1815fb050c76c540b5c8cd82791203176d2a220
topgirl2410 <maria.topgirl82@gmail.com>
2022-03-23 23:25:26 +0100

    creando slider

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+
+window.swiper = new Swiper({
+    el: '.slider__contenedor',
+    slideClass: 'slider__slide',
+    createElements: true,
+    autoplay: {
+        delay: 5000
+    },
+    loop: true,
+    //pagination: true,
+    navigation: true
+});
